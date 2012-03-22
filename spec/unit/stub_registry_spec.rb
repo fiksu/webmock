71ce3ed6553dfb395aaaa54d78e14c3795ca91b4
Vokhmin Alexey V <avokhmin@fiksu.com>
2012-03-22 20:15:51 +0400

    #24939983: added specs for reset_stub method

--- spec/unit/stub_registry_spec.rb
+++ spec/unit/stub_registry_spec.rb
@@ -21,6 +21,57 @@ describe WebMock::StubRegistry do
     end
   end
 
+  describe "reset_stub" do
+    let(:stub11) { WebMock::RequestStub.new(:get, "www.example.com/123/?x=1") }
+    let(:stub12) { WebMock::RequestStub.new(:get, "www.example.com/123/?x=2") }
+    let(:stub13) { WebMock::RequestStub.new(:post, "www.example.com/123/?x=1") }
+    let(:stub14) { WebMock::RequestStub.new(:post, "www.example.com/123/?x=2") }
+
+    let(:stub21) { WebMock::RequestStub.new(:get, "www.example.com/234/?x=1") }
+    let(:stub22) { WebMock::RequestStub.new(:get, "www.example.com/234/?x=2") }
+    let(:stub23) { WebMock::RequestStub.new(:post, "www.example.com/234/?x=1") }
+    let(:stub24) { WebMock::RequestStub.new(:post, "www.example.com/234/?x=2") }
+
+    let(:instance) { WebMock::StubRegistry.instance }
+    
+    before do
+      [stub11, stub12, stub13, stub14,
+        stub21, stub22, stub23, stub24].each do |stub|
+        instance.register_request_stub(stub)
+      end
+    end
+
+    it "should return true if stub request removed" do
+      instance.reset_stub(:get, /123\/.*=2$/).should be_true
+    end
+
+    it "should return false if stub request not found" do
+      instance.reset_stub(:get, /123\/.*=3$/).should be_false
+    end
+
+    describe "checks reset stub request by uri_regexp" do
+      before { instance.reset_stub(:get, /123\/.*=2$/) }
+      subject { instance.request_stubs }
+      it { should have(7).items }
+      it { should_not include(stub12) }
+    end
+
+    describe "checks reset collection of stub" do
+      before { instance.reset_stub(:get, /\/123\//) }
+      subject { instance.request_stubs }
+      it { should have(6).items }
+      it { should_not include(stub11, stub11) }
+    end
+
+    describe "checks reset stub request by uri_regexp and request method" do
+      before { instance.reset_stub(:post, /123\/.*=2$/) }
+      subject { instance.request_stubs }
+      it { should have(7).items }
+      it { should_not include(stub14) }
+    end
+
+  end
+
   describe "registering and reporting registered requests" do
 
     it "should return registered stub" do
